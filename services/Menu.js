1903f50009a8446a93fe1c4798300f4b3139cf2d
Simo Ahlqvist <p19497@edu.sasky.fi>
2025-10-03 14:11:33 +0300

    Add ProductItem component and enhance MenuPage rendering logic. Create reactive Store proxy, add ProductItem, add  getProductionById

--- services/Menu.js
+++ services/Menu.js
@@ -3,3 +3,17 @@ import API from "./API.js"; // add .js
 export async function loadData() {
   app.store.menu = await API.fetchMenu();
 }
+
+export async function getProductionById(id) {
+  if ((app.store.menu = null)) {
+    await loadData();
+  }
+  for (let c of app.store.menu) {
+    for (let p of c.products) {
+      if (p.id == id) {
+        return p;
+      }
+    }
+  }
+  return null;
+}
